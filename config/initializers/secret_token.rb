ec171cbfec6335f6590f46b5c23988612135f0b3
blueplanet <erguolinge@gmail.com>
2013-05-27 12:40:40 +0900

    Init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+GroupEvents::Application.config.secret_key_base = '055ce1f789b7879316b138dc7a9cec023f9ffd856e133741338aabc3bde29c1f3eb884ea12c875b5c74793cc5097a3fe02224dc9397698dd45fbf12442a69289'
