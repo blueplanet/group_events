c3ce26b80a4fa601a236f44f31567e24f07b1c45
blueplanet <erguolinge@gmail.com>
2013-06-08 13:34:50 +0900

    Add member name to group page

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,4 +1,5 @@
 class Group < ActiveRecord::Base
 
   has_many :events
+  has_many :members, :class_name => "User", :foreign_key => "group_id"
 end
---
+++ spec/features/guest_see_group_members_spec.rb
@@ -0,0 +1,19 @@
+require 'spec_helper'
+
+feature 'ゲストは、グループのメンバー一覧を見える' do
+  let(:group) { FactoryGirl.create(:seq_group) }
+
+  before do
+    10.times do |i|
+      group.members.create name: "user #{i}", img_url: "icon_#{i}.jpg"
+    end
+
+    visit group_path(group)
+  end 
+
+  scenario 'グループメンバーの名称が表示される' do
+    group.members.each do |member|
+      page.should have_content member.name
+    end
+  end
+end
