2bdac45eb90c8d4acbdefebf2bc035b9259a678f
blueplanet <erguolinge@gmail.com>
2013-06-08 19:20:44 +0900

    Change join logic

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,20 +1,18 @@
 class GroupsController < ApplicationController
   before_action :set_group, only: [:show, :join]
+  
   def index
     @groups = Group.all
   end
 
   def join
-    if current_user
-      @group.members << current_user
-      @group.save
-    end
+    @group.members << current_user if current_user
 
     redirect_to group_path(@group)
   end
 
   private
-    def set_group
-      @group = Group.find(params[:id])
-    end
+  def set_group
+    @group = Group.find(params[:id])
+  end
 end
