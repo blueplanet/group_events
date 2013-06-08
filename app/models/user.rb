8b119749c190a784ceb409d983315bf230afa81c
blueplanet <erguolinge@gmail.com>
2013-06-08 18:48:42 +0900

    Add spec to user#groups, group#members

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ActiveRecord::Base
   has_many :group_users
-  has_many :group, through: :group_users
+  has_many :groups, through: :group_users
 
   def self.create_with_omniauth(auth)
     create do |user|
