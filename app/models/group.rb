1730f6a731c01e2234932da7a583e68dc8e94708
blueplanet <erguolinge@gmail.com>
2013-06-11 19:18:05 +0900

    Add uniqueness for name

--- app/models/group.rb
+++ app/models/group.rb
@@ -4,4 +4,5 @@ class Group < ActiveRecord::Base
   has_many :members, through: :group_users, source: :user
 
   validates :name, presence: {message: "名称を入力してください"} 
+  validates :name, uniqueness: {message: "名称は既に使われています"}
 end
