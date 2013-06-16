638458dad99142ea9f788531ce4c9c9398191ec9
blueplanet <erguolinge@gmail.com>
2013-06-16 16:12:41 +0900

    Add edit join_type of event

---
+++ spec/features/group_member_can_edit_join_type_spec.rb
@@ -0,0 +1,41 @@
+require 'spec_helper'
+
+feature 'グループメンバーは、参加状況を編集出来る' do
+  let(:group) { FactoryGirl.create(:group_event) }
+  let(:event) { group.events.sample }
+
+  include_context 'ログインしている'
+  include_context 'グループメンバーである'
+
+  before { visit group_event_path(group, event) }
+
+  context "参加している場合" do
+    background { click_link '参加' }
+
+    scenario '欠席と微妙リンクが表示される' do
+      page.should_not have_link '参加'
+      page.should have_link '欠席'
+      page.should have_link '微妙'
+    end
+  end
+
+  context "欠席している場合" do
+    background { click_link '欠席'}
+
+    scenario '参加と微妙リンクが表示される' do
+      page.should have_link '参加'
+      page.should_not have_link '欠席'
+      page.should have_link '微妙'
+    end
+  end
+
+  context "微妙の場合" do
+    background { click_link '微妙' }
+
+    scenario '参加と欠席リンクが表示される' do
+      page.should have_link '参加'
+      page.should have_link '欠席'
+      page.should_not have_link '微妙'
+    end
+  end
+end
